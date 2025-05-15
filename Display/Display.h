c431c440e46baca9d5aedcd48899507d2b11a92f
Jerimaya Flores <137690003+LazyXTwo@users.noreply.github.com>
2025-05-15 11:37:04 +0700

    Add files via upload

---
+++ Display/Display.h
@@ -0,0 +1,13 @@
+void intro(){
+  std::cout << R"(
+   _____  _____  ____  _____  ______  _______     __
+  / ____|/ ____|/ __ \|  __ \|  ____|/ ____\ \   / /
+ | |    | (___ | |  | | |__) | |__  | (___  \ \_/ / 
+ | |     \___ \| |  | |  ___/|  __|  \___ \  \   /  
+ | |____ ____) | |__| | |    | |____ ____) |  | |   
+  \_____|_____/ \____/|_|    |______|_____/   |_|   
+
+  Hello, Welcome to CSOPESY commandline!
+  Type 'exit' to quit, 'clear' to clear the screen, 'command' to show all comands
+)"<< '\n';
+}
